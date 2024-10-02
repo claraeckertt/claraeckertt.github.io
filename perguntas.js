a472e434a43d34a54f168fec1e96660dc499a298
claraeckertt <160048335+claraeckertt@users.noreply.github.com>
2024-10-02 14:50:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,58 @@
+criaCartao(
+    'Matematica',
+    'Qual a raiz quadrada de 36?',
+    'A raiz quadrada de 36 e 6'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Tocantins?',
+    'A capital do Tocantins e Palmas'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Matematica',
+    'Quanto e 5 + 7?',
+    '5 + 7 e igual a 12'
+)
+
+criaCartao( 
+    'Artes',
+    'Quem pintou a noite estrelada',
+    'Van Gogh'
+)
+
+criaCartao(
+    'Esportes',
+    'Quantos pontos para terminarem uma partida de voleibol',
+    'E preciso de 25 pontos',
+    
+)
+
+criaCartao(
+    'Matematica',
+    'Quanto e 9 x 8?',
+    '9 x 8 e 72'
+)
+
+criaCartao(
+    'Ciencias',
+    'Qual a formula da agua?',
+    'H2O'
+)
+
+criaCartao(
+    'Ciencias',
+    'Qual a formula do oxigenio?',
+    'O2'
+)
+
+
+
+
